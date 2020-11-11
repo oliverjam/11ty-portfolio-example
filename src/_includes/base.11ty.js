49060d935f80e6bea44d6c401318722d9b9a1343
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-11-11 19:00:35 +0000

    Add homepage content and styles

--- src/_includes/base.11ty.js
+++ src/_includes/base.11ty.js
@@ -9,13 +9,13 @@ exports.render = (data) => {
         <link href="/assets/css/base.css" rel="stylesheet">
       </head>
       <body>
-        <header>
+        <header class="site-header">
           <a href="/" aria-label="Home">
             <svg width="32" height="32" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21a4 4 0 01-4-4V5a2 2 0 012-2h4a2 2 0 012 2v12a4 4 0 01-4 4zm0 0h12a2 2 0 002-2v-4a2 2 0 00-2-2h-2.343M11 7.343l1.657-1.657a2 2 0 012.828 0l2.829 2.829a2 2 0 010 2.828l-8.486 8.485M7 17h.01" />
             </svg>
           </a>
-          <nav>
+          <nav style="--delay: 0.35s">
             <a href="/blog">Blog</a>
           </nav>
         </header>
--- src/index.11ty.js
+++ src/index.11ty.js
@@ -4,7 +4,12 @@ exports.data = {
 };
 
 exports.render = () => {
-  return `
-    <h1>Home</h1>
+  return /*html*/ `
+    <link href="/assets/css/home.css" rel="stylesheet">
+    <div class="big-sell">
+      <img width="500" src="/assets/images/undraw_code_thinking.svg" alt="">
+      <h1 style="--delay: 0.35s">I am a web developer</h1>
+      <p style="--delay: 0.7s">Insert outlandish claims of competence here</p>
+    </div>
   `;
 };
