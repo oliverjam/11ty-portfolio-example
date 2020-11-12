38f2273af45be6558335500c5f94741e35ff03ee
Oliver Phillips <oliverjamesphillips@gmail.com>
2020-11-12 00:00:59 +0000

    Add moon emoji favicon

--- src/_includes/base.11ty.js
+++ src/_includes/base.11ty.js
@@ -7,6 +7,7 @@ exports.render = (data) => {
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>${data.title} | ${data.site.title}</title>
         <link href="/assets/css/base.css" rel="stylesheet">
+        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%22256%22 height=%22256%22 viewBox=%220 0 100 100%22><text x=%2250%%22 y=%2250%%22 dominant-baseline=%22central%22 text-anchor=%22middle%22 font-size=%2290%22>🌝</text></svg>" />
       </head>
       <body>
         <div class="page">
